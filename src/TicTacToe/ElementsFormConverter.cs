29842f1dc40398241b2a56dd34429e13ef6e37b7
dj_serega <galyuk.sergey@gmail.com>
2018-06-08 01:17:52 +0300

    initialize project

---
+++ src/TicTacToe/ElementsFormConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TicTacToe
+{
+    public class ElementsFormConverter : MarkupExtension, IValueConverter
+    {
+        private static ElementsFormConverter _instance;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = ToDouble(value) - ToDouble(parameter);
+            return result < 0 ? value : result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ToDouble(value) + ToDouble(parameter);
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return _instance ?? (_instance = new ElementsFormConverter());
+        }
+
+        private Double ToDouble(object value)
+        {
+            return System.Convert.ToDouble(value);
+        }
+    }
+}
